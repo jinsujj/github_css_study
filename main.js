9892123fb1a723b4e5ea8d888506e625cd0d7f94
jinsujj <wlstncjs1234@naver.com>
2021-12-01 16:48:33 +0900

    update

--- main.js
+++ main.js
@@ -10,10 +10,23 @@
         toggleElements();
     });
 
+    window.addEventListener('resize', function() {
+        // console.log('Resized');
+        if(window.innerWidth > 1024){
+            offElements();
+        }
+    });
+
     function toggleElements(){
         [].forEach.call($toggles, function (toggle) {
             // toggle 이라는 클래스를 가진 요소에다가 on 을 추가해주겠다.
             toggle.classList.toggle('on');
         });
+    }
+
+    function offElements(){
+        [].forEach.call($toggles, function(toggle){
+            toggle.classList.remove('on');
+        })
     }
 })(window, document)
